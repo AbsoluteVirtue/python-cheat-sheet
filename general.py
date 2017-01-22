96a6066384eea7c2fde1240525aafd36dc2b2544
Leo <3hitemouse@gmail.com>
2017-01-22 22:59:17 +0200

    excel data

--- general.py
+++ general.py
@@ -241,3 +241,27 @@ def list_excel_columns(file):
     sheet = wb.get_sheet_by_name('Sheet1')
     for cellObj in sheet.columns[1]:
         print(cellObj.value)
+
+
+def print_out_excel_data(file):
+    import openpyxl, pprint
+    print('Opening workbook...')
+    wb = openpyxl.load_workbook(file)
+    sheet = wb.get_sheet_by_name('Population by Census Tract')
+    countyData = {}
+
+    for row in range(2, sheet.max_row + 1):
+        state = sheet['B' + str(row)].value
+        county = sheet['C' + str(row)].value
+        pop = sheet['D' + str(row)].value
+
+        countyData.setdefault(state, {})
+        countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+        countyData[state][county]['tracts'] += 1
+        countyData[state][county]['pop'] += int(pop)
+
+    print('Writing results...')
+    resultFile = open('census.py', 'w')
+    resultFile.write('allData = ' + pprint.pformat(countyData))
+    resultFile.close()
+    print('Done.')
